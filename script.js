835a3c8d64d961f4723406e975d1adefc7126021
bindasskarate <aligaming8976@gmail.com>
2026-08-04 23:54:39 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+/* ===== HOME MENU ONLY ===== */
+
+(function () {
+
+    const menuToggle = document.getElementById("menuToggle");
+    const navMenu = document.getElementById("navMenu");
+
+    if (menuToggle && navMenu) {
+
+        menuToggle.addEventListener("click", function () {
+            navMenu.classList.toggle("open");
+        });
+
+    }
+
+})();
